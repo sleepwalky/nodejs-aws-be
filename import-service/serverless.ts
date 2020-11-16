a2fc9a1183904bb9867123d9907b3cdcf71f86c7
Alex Kleshchukevich <keeweery+github@gmail.com>
2020-11-16 19:29:21 +0300

    implement signed url

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -11,34 +11,52 @@ const serverlessConfiguration: Serverless = {
   custom: {
     webpack: {
       webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
+      includeModules: true,
+    },
   },
   // Add the serverless-webpack plugin
   plugins: ['serverless-webpack'],
   provider: {
     name: 'aws',
     runtime: 'nodejs12.x',
+    profile: 'rss',
+    region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
+    iamRoleStatements: [
+      {
+        Sid: 'Stmt1605539376440',
+        Effect: 'Allow',
+        Action: 's3:PutObject',
+        Resource: 'arn:aws:s3:::store-imported-products',
+      },
+    ],
   },
   functions: {
-    hello: {
-      handler: 'handler.hello',
+    importProductsFile: {
+      handler: 'handlers.importProductsFile',
       events: [
         {
           http: {
             method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+            path: 'import',
+            cors: true,
+            request: {
+              parameters: {
+                querystrings: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+  },
+};
 
 module.exports = serverlessConfiguration;
